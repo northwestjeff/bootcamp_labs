03e257b2cf3141f9bc3702af8f50bc5f412ba137
Jeff Bailey <northwestjeff@gmail.com>
2017-10-02 09:39:29 -0700

    more css work

--- python/sports_teams.py
+++ python/sports_teams.py
@@ -1,40 +1,39 @@
-# from bs4 import BeautifulSoup
-# from urllib.request import urlopen
-# import urllib3
-# import certifi
-# import requests
-# from lxml import html
-#
-# # http = urllib3.PoolManager(
-# #     cert_reqs='CERT_REQUIRED',
-# #     ca_certs=certifi.where())
-#
-# teams_page = "https://en.wikipedia.org/wiki/List_of_professional_sports_teams_in_the_United_States_and_Canada"
-#
-# page = urlopen(teams_page)
-#
-# soup = BeautifulSoup(page, 'html.parser')
-#
-# teams = soup.find_all('b')
-#
-# print(teams)
-#
-
 from wikitables import import_tables
+import sqlite3
 
-teams = 'Major professional sports teams of the United States and Canada'
-italy = 'List of cities in Italy'
-
-
-teams_dict = import_tables(teams)
-
-for row in teams_dict[0].rows:
-    print(row)
-    # print("{}, {}, {}".format(row["Team"], row["City"], row["League"]))
-
-
-
-# italy_cities = import_tables(italy)
 #
-# for row in italy_cities[0].rows:
-#     print("{}, population: {}".format(row["City"], row["2001 Census"]))
+# # pulls data from wikipedia table
+# teams = 'Major professional sports teams of the United States and Canada'
+#
+# teams_dict = import_tables(teams)
+# data = []
+#
+# # creates a list of dictionary elements
+# for row in teams_dict[0].rows:
+#     data.append(row)
+#
+# # converts the dictionary values from objects to strings
+# for i in data:
+#     for k in i:
+#         i[k] = str(i[k])
+#
+#
+# # create a new SQLlite database and generates a cursor for SQL execution
+# conn = sqlite3.connect('sports_teams_database.sqlite')
+# cur = conn.cursor()
+#
+#
+# cur.execute('''CREATE TABLE sports_table1(id INTEGER PRIMARY KEY, team TEXT, city TEXT, league TEXT)''')
+#
+# for i in data:
+#     cur.execute('''INSERT INTO sports_table1(team, city, league) VALUES(:Team, :City, :League)''', i)
+#
+#
+# conn.commit()
+# conn.close()
+
+nba_players = 'List of current NBA team rosters'
+
+nba_players_dict = import_tables(nba_players)
+
+print(nba_players_dict)
