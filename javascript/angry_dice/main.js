182e374fe2a16e19d81c2a09efeaf63b08375769
Jeff Bailey <northwestjeff@gmail.com>
2017-10-12 11:40:30 -0700

    finishing touches on Dice Game

--- javascript/angry_dice/main.js
+++ javascript/angry_dice/main.js
@@ -21,10 +21,8 @@ function Die(id) {
     this.held = false;
     this.value = 3;
     this.roll = function() {
-        console.log("ROlling!!!")
         if (this.held === false) {
             this.value = Math.floor(Math.random() * 6) + 1;
-            console.log("die" + this.number + " random" + this.value )
         }
     }
 }
@@ -32,6 +30,30 @@ function Die(id) {
 // Starts the game with blank Holds
 $('.btn--radius')[0].classList.remove("held")
 $('.btn--radius')[0].innerText = "HOLD"
+
+function setup() {
+    var hold = $('.btn--radius');
+    var instruct = $('#instructionsList')
+    hold[0].classList.remove("held")
+    hold[0].innerText = "HOLD"
+    instruct.empty()
+    instruct.append('<div class="box play" id="roundone" >Round One: Roll a combination of 1 and 2</div>')
+    instruct.append('<div class="box play" id="roundtwo" >Round Two: Roll an Angry Face and a 4</div>')
+    instruct.append('<div class="box play" id="roundthree" >Round Three: Roll a combination of 5 and 6</div>')
+    $("#roundone").css(
+        "font-weight", "900",
+    );
+    $("#roundtwo").css(
+        "background", "gray"
+    );
+    $("#roundthree").css(
+        "background", "gray"
+    )
+
+}
+
+setup()
+
 
 // checks for the word "Held" to add the Held class or not
 function holdCheck(elem){
@@ -46,58 +68,41 @@ function holdCheck(elem){
 
 // Makes the Die.held true or false
 function holdDie() {
-    if ($('#hold1')[0].classList.value.includes("held") === true) {
-        die1.held = true;
-    } else {die1.held = false
-    }
-    if ($('#hold2')[0].classList.value.includes("held") === true) {
-        die2.held = true;
-    } else {die2.held = false
-    }
-    // if (elem.id === "hold1" && elem.classList.value.includes("held")) {
-    //     die1.held = true;
-    // } else {
-    //     die1.held = false;
-    // }
-    // console.log("die1 held is " + die1.held)
-    // if (elem.id === "hold2" && elem.classList.value.includes("held")) {
-    //     die2.held = true;
-    // } else {
-    //     die2.held = false;
-    // }
-    // console.log("die2 held is " + die2.held)
+    die1.held = $('#hold1')[0].classList.value.includes("held") === true;
+    die2.held = $('#hold2')[0].classList.value.includes("held") === true;
+
 }
 
 // ROLL
 $('#roll').click( function() {
-    console.log("click ROLL");
     die1.roll();
     die2.roll();
     updateImage();
-    roundCheck();
-    doubleAngry();
-
+    if (die1.value === 3 && die2.value === 3) {
+        round1.complete = false;
+        round2.complete = false;
+        round3.complete = false;
+        setup()
+    } else {
+        roundCheck();
+        // doubleAngry();
+    }
 });
 
 $('.btn--radius').click(function () {
-    // if(this) {
-    //
-    // } else {
     var num = $(this).attr('id').slice(-1);
-    if (die + num === )
-    holdCheck(this)
-    holdDie(this)
-
-    // }
+    if (num === "1" && die1.value === 6) {
+    } else if (num === "2" && die2.value === 6) {
+    } else {
+        holdCheck(this);
+        holdDie(this)
+    }
 });
 
 function updateImage() {
     $('#die1').find('> img').attr("src", "img/" + die1.value + ".png")
     $('#die2').find('> img').attr("src", "img/" + die2.value + ".png")
 }
-
-// strikethrough successful rounds
-// check for double 3s
 
 
 function roundOne() {
@@ -109,40 +114,61 @@ function roundOne() {
 function roundTwo() {
     if (die1.value === 4 || die2.value === 4) {
         if (die1.value === 3 || die2.value === 3) {
-            round2.complete = true
+            if (round1.complete === true) {
+                round2.complete = true
+            }
         }
     }
 }
 
-
 function roundThree() {
-    if (die1.value + die2.value === round3.sum) {
+    if (die1.value + die2.value === round3.sum && round2.complete === true) {
         round3.complete = true
     }
 }
-
 
 function roundCheck() {
     roundOne()
     roundTwo()
     roundThree()
     if (round1.complete === true) {
-        $('#instructionsList').find("li")[0].style.textDecoration = "line-through";
+        $('#roundone').css({
+            "backgroundColor": "green",
+            "color": "white"
+        })
+        $('#roundtwo').css({
+            "backgroundColor": "white",
+        })
     }
     if (round1.complete === true && round2.complete === true) {
-        $('#instructionsList').find("li")[1].style.textDecoration = "line-through";
+        $('#roundtwo').css({
+            "backgroundColor": "green",
+            "color": "white"
+        })
+        $('#roundthree').css({
+            "backgroundColor": "white",
+        })
     }
     if (round2.complete === true && round3.complete === true) {
-        $('#instructionsList').find("li")[2].style.textDecoration = "line-through";
-        $
+        $('#roundthree').css({
+            "backgroundColor": "green",
+            "color": "white"
+        })
     }
 }
 
 
-function doubleAngry() {
-    if (die1.value === 3 && die2.value === 3) {
-        round1.complete = false;
-        round2.complete = false;
-        round3.complete = false;
-    }
-}
+
+
+
+// function doubleAngry() {
+//     if (die1.value === 3 && die2.value === 3) {
+//         round1.complete = false;
+//         round2.complete = false;
+//         round3.complete = false;
+//     }
+// }
+
+
+// TODO Play again
+// TODO animate roll
